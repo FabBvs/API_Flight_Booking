ca95b2e12d69b5c7b51579f5969465b9ea9547cb
FabBvs <fab.beauvisage@gmail.com>
2023-11-21 15:20:15 +0100

    AvionResource

--- src/main/java/beans/Avion.java
+++ src/main/java/beans/Avion.java
@@ -2,6 +2,10 @@ package beans;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,15 +21,21 @@ public class Avion extends PanacheEntity {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "planes_sequence_in_java_code")
     private Long id;
 
+    @NotBlank(message = "La compagnie ne peut pas être nulle !")
     @Column(nullable = false)
-    private String operator;
+    private String operator; //Compagnie de l'avion !
 
+    @NotBlank(message = "Le modèle de l'avion ne peut pas être nul !")
     @Column(nullable = false)
-    private String model;
+    private String model; //Modèle de l'avion !
 
+    @NotBlank(message = "L'immatriculation de l'avion ne peut pas être nulle !")
+    @Size(max = 6, message = "L'immatriculation à un maximum de 6 caractères !")
     @Column(nullable = false, unique = true)
-    private String registration; //Immatriculation
+    private String registration; //Immatriculation !
 
+    @NotNull(message = "La capacité ne peut pas être nulle !")
     @Column(nullable = false)
-    private Integer capacity;
+    @Min(value = 6, message = "La capacité doit être au minimum de 6 !")
+    private Integer capacity; //Capacité de l'avion !
 }
