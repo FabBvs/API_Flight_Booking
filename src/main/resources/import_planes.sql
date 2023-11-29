2b6e750a0dac8063e065a239ad689ed4d87de51c
FabBvs <fab.beauvisage@gmail.com>
2023-11-29 17:19:31 +0100

    A

--- src/main/resources/import_planes.sql
+++ src/main/resources/import_planes.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-INSERT INTO planes(id, operator, model, registration, capacity) VALUES(NEXTVAL('planes_sequence_in_database'), 'AirbusIndustrie', 'AIRBUS A380', 'F-ABCD', 10);
-INSERT INTO planes(id, operator, model, registration, capacity) VALUES(NEXTVAL('planes_sequence_in_database'), 'Boeing', 'BOEING CV2', 'F-AZER', 15);
+INSERT INTO planes(id, operator, model, registration, capacity) VALUES(NEXTVAL('planes_sequence_in_database'), 'AirbusIndustrie', 'AIRBUS A380', 'P1', 545);
+INSERT INTO planes(id, operator, model, registration, capacity) VALUES(NEXTVAL('planes_sequence_in_database'), 'Boeing', 'BOEING 788', 'P2', 279);
 
 COMMIT;
